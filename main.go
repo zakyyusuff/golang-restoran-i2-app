6252c4e83e93b02b512e2873cff0855f1790825b
unknown <zakymuhammadyusuf@gmail.com>
2023-05-12 08:55:41 +0700

    test 3

--- main.go
+++ main.go
@@ -16,6 +16,12 @@ import (
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
+	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
+	// url := os.Getenv("restaurant")
+	// clientOptions := options.Client().ApplyURI(url)
+	// client, err := mongo.Connect(context.TODO(), clientOptions)
+	// port := os.Getenv(clientOptions)
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -36,3 +42,44 @@ func main() {
 
 	router.Run(":" + port)
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// package main
+
+// import (
+// 	"os"
+
+// 	"golang-restaurant-management/database"
+
+// 	middleware "golang-restaurant-management/middleware"
+// 	routes "golang-restaurant-management/routes"
+
+// 	"github.com/gin-gonic/gin"
+
+// 	"go.mongodb.org/mongo-driver/mongo"
+// )
+
+// var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+// func main() {
+// 	port := os.Getenv("PORT")
+
+// 	if port == "" {
+// 		port = "8000"
+// 	}
+
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	routes.UserRoutes(router)
+// 	router.Use(middleware.Authentication())
+
+// 	routes.FoodRoutes(router)
+// 	routes.MenuRoutes(router)
+// 	routes.TableRoutes(router)
+// 	routes.OrderRoutes(router)
+// 	routes.OrderItemRoutes(router)
+// 	routes.InvoiceRoutes(router)
+
+// 	router.Run(":" + port)
+// }
