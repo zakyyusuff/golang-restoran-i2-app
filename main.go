5b37faaf1e74721dd09b61cedea52da42bd72477
unknown <zakymuhammadyusuf@gmail.com>
2023-05-14 20:36:19 +0700

    add Procfile

--- main.go
+++ main.go
@@ -15,36 +15,13 @@ import (
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
-// func main() {
-// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
-// 	// url := os.Getenv("restaurant")
-// 	// clientOptions := options.Client().ApplyURI(url)
-// 	// client, err := mongo.Connect(context.TODO(), clientOptions)
-// 	// port := os.Getenv(clientOptions)
-
-// 	port := os.Getenv("PORT")
-
-// 	if port == "" {
-// 		port = "8000"
-// 	}
-
-// 	router := gin.New()
-// 	router.Use(gin.Logger())
-// 	routes.UserRoutes(router)
-// 	router.Use(middleware.Authentication())
-
-// 	routes.FoodRoutes(router)
-// 	routes.MenuRoutes(router)
-// 	routes.TableRoutes(router)
-// 	routes.OrderRoutes(router)
-// 	routes.OrderItemRoutes(router)
-// 	routes.InvoiceRoutes(router)
-
-// 	router.Run(":" + port)
-// }
-
-// //////////////////////////////////////////////////////////////////////////////////////////
 func main() {
+	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
+	// url := os.Getenv("restaurant")
+	// clientOptions := options.Client().ApplyURI(url)
+	// client, err := mongo.Connect(context.TODO(), clientOptions)
+	// port := os.Getenv(clientOptions)
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -54,23 +31,46 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
-
 	router.Use(middleware.Authentication())
 
-	// API-2
-	router.GET("/api-1", func(c *gin.Context) {
-
-		c.JSON(200, gin.H{"success": "Access granted for api-1"})
-
-	})
-
-	// API-1
-	router.GET("/api-2", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Access granted for api-2"})
-	})
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
 
 	router.Run(":" + port)
 }
+
+// //////////////////////////////////////////////////////////////////////////////////////////
+// func main() {
+// 	port := os.Getenv("PORT")
+
+// 	if port == "" {
+// 		port = "8000"
+// 	}
+
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	routes.UserRoutes(router)
+
+// 	router.Use(middleware.Authentication())
+
+// 	// API-2
+// 	router.GET("/api-1", func(c *gin.Context) {
+
+// 		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+
+// 	})
+
+// 	// API-1
+// 	router.GET("/api-2", func(c *gin.Context) {
+// 		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+// 	})
+
+// 	router.Run(":" + port)
+// }
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
