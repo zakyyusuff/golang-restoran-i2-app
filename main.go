e0df15f39db3da1798529830fccd5713b0833c57
zakymuhammadyusuf@gmail.com <zakymuhammadyusuf@gmail.com>
2023-05-16 23:09:50 +0700

    puha

--- main.go
+++ main.go
@@ -43,7 +43,7 @@
 // 	router.Run(":" + port)
 // }
 
-// //////////////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////////////		1
 package main
 
 import (
@@ -72,7 +72,7 @@ func main() {
 		port = "8000"
 	}
 
-	router := gin.New()
+	router := gin.Default()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
@@ -86,6 +86,76 @@ func main() {
 
 	router.Run(":" + port)
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////		2 uji coba
+
+// package main
+
+// import (
+// 	"fmt"
+// 	middleware "golang-restaurant-management/middleware"
+// 	routes "golang-restaurant-management/routes"
+// 	"net/http"
+// 	"os"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// // func Handler(w http.ResponseWriter, r *http.Request) {
+// // 	server := New()
+
+// // 	server.get("/",func(context *Context) {
+// // 		context.JSON(200,H{
+// // 			"message":"Hello From Vercel !!!"})
+// // 	})
+
+// // 	server.get("/users",func(context *Context) {
+// // 		name := context.Query("name")
+// // 		if name = "" {
+// // 			context.JSON(400,H{
+// // 				"message":"Hello From Vercel !!!"})
+// // 		}else{
+// // 			context.JSON(400,H{
+// // 				"message":fmt.Sprintf("hai %s", name)})
+// // 		}
+// // 	})
+// // }
+
+// func main(w http.ResponseWriter, r *http.Request) {
+// 	server := database()
+
+// 	server.get("/", func(context *Context) {
+// 		context.JSON(200, H{
+// 			"message": "Hello From Vercel !!!"})
+// 	})
+
+// 	fmt.Println("Server is running...")
+// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/test")
+// 	// url := os.Getenv("restaurant")
+// 	// clientOptions := options.Client().ApplyURI(url)
+// 	// client, err := mongo.Connect(context.TODO(), clientOptions)
+// 	// port := os.Getenv(clientOptions)
+
+// 	port := os.Getenv("PORT")
+
+// 	if port == "" {
+// 		port = "8000"
+// 	}
+
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	routes.UserRoutes(router)
+// 	router.Use(middleware.Authentication())
+
+// 	routes.FoodRoutes(router)
+// 	routes.MenuRoutes(router)
+// 	routes.TableRoutes(router)
+// 	routes.OrderRoutes(router)
+// 	routes.OrderItemRoutes(router)
+// 	routes.InvoiceRoutes(router)
+
+// 	router.Run(":" + port)
+// }
 
 // //////////////////////////////////////////////////////////////////////////////////////////
 // import (
