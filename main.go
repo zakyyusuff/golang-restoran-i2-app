653cb1d9949b8d41bbbfd18508ce56dbc93edc03
zakymuhammadyusuf@gmail.com <zakymuhammadyusuf@gmail.com>
2023-05-17 00:40:22 +0700

    main

--- main.go
+++ main.go
@@ -89,73 +89,73 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////		2 uji coba
 
-package main
+// package main
 
-import (
-	"fmt"
-	middleware "golang-restaurant-management/middleware"
-	routes "golang-restaurant-management/routes"
-	"net/http"
-	"os"
+// import (
+// 	"fmt"
+// 	middleware "golang-restaurant-management/middleware"
+// 	routes "golang-restaurant-management/routes"
+// 	"net/http"
+// 	"os"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func main() {
+// func main() {
 
-	fmt.Println("Server is running...")
-	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/test")
-	// url := os.Getenv("restaurant")
-	// clientOptions := options.Client().ApplyURI(url)
-	// client, err := mongo.Connect(context.TODO(), clientOptions)
-	// port := os.Getenv(clientOptions)
+// 	fmt.Println("Server is running...")
+// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/test")
+// 	// url := os.Getenv("restaurant")
+// 	// clientOptions := options.Client().ApplyURI(url)
+// 	// client, err := mongo.Connect(context.TODO(), clientOptions)
+// 	// port := os.Getenv(clientOptions)
 
-	port := os.Getenv("PORT")
+// 	port := os.Getenv("PORT")
 
-	if port == "" {
-		port = "8000"
-	}
+// 	if port == "" {
+// 		port = "8000"
+// 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	routes.UserRoutes(router)
-	router.Use(middleware.Authentication())
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	routes.UserRoutes(router)
+// 	router.Use(middleware.Authentication())
 
-	routes.FoodRoutes(router)
-	routes.MenuRoutes(router)
-	routes.TableRoutes(router)
-	routes.OrderRoutes(router)
-	routes.OrderItemRoutes(router)
-	routes.InvoiceRoutes(router)
+// 	routes.FoodRoutes(router)
+// 	routes.MenuRoutes(router)
+// 	routes.TableRoutes(router)
+// 	routes.OrderRoutes(router)
+// 	routes.OrderItemRoutes(router)
+// 	routes.InvoiceRoutes(router)
 
-	router.Run(":" + port)
-}
-
-func Handler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello From Vercel !!!",
-	})
-}
-
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// 	server := New()
-
-// 	server.get("/", func(context *Context) {
-// 		context.JSON(200, H{
-// 			"message": "Hello From Vercel !!!"})
-// 	})
-
-// 	// server.get("/users",func(context *Context) {
-// 	// 	name := context.Query("name")
-// 	// 	if name = "" {
-// 	// 		context.JSON(400,H{
-// 	// 			"message":"Hello From Vercel !!!"})
-// 	// 	}else{
-// 	// 		context.JSON(400,H{
-// 	// 			"message":fmt.Sprintf("hai %s", name)})
-// 	// 	}
-// 	// })
+// 	router.Run(":" + port)
 // }
+
+// func Handler(c *gin.Context) {
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"message": "Hello From Vercel !!!",
+// 	})
+// }
+
+// // func Handler(w http.ResponseWriter, r *http.Request) {
+// // 	server := New()
+
+// // 	server.get("/", func(context *Context) {
+// // 		context.JSON(200, H{
+// // 			"message": "Hello From Vercel !!!"})
+// // 	})
+
+// // 	// server.get("/users",func(context *Context) {
+// // 	// 	name := context.Query("name")
+// // 	// 	if name = "" {
+// // 	// 		context.JSON(400,H{
+// // 	// 			"message":"Hello From Vercel !!!"})
+// // 	// 	}else{
+// // 	// 		context.JSON(400,H{
+// // 	// 			"message":fmt.Sprintf("hai %s", name)})
+// // 	// 	}
+// // 	// })
+// // }
 
 // //////////////////////////////////////////////////////////////////////////////////////////	uji coba 3
 // package main
@@ -194,3 +194,35 @@ func Handler(c *gin.Context) {
 
 // 	router.Run(":" + port)
 // }
+//////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Server is running...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/", Handler)
+
+	router.Run(":" + port)
+}
+
+func Handler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello From Vercel !!!",
+	})
+}
