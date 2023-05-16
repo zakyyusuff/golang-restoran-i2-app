8c6c0d92d336bd0062d255f76e97de3f85f80a54
zakymuhammadyusuf@gmail.com <zakymuhammadyusuf@gmail.com>
2023-05-17 00:33:30 +0700

    main

--- main.go
+++ main.go
@@ -157,98 +157,40 @@ func Handler(c *gin.Context) {
 // 	// })
 // }
 
-// //////////////////////////////////////////////////////////////////////////////////////////
-// import (
-// 	"os"
-
-// 	middleware "golang-restaurant-management/middleware"
-// 	routes "golang-restaurant-management/routes"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// func main() {
-// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
-// 	router := gin.New()
-// 	router.Use(gin.Logger())
-// 	routes.UserRoutes(router)
-// 	router.Use(middleware.Authentication())
-
-// 	routes.FoodRoutes(router)
-// 	routes.MenuRoutes(router)
-// 	routes.TableRoutes(router)
-// 	routes.OrderRoutes(router)
-// 	routes.OrderItemRoutes(router)
-// 	routes.InvoiceRoutes(router)
-
-// }
-
-// //////////////////////////////////////////////////////////////////////////////////////////
-// func main() {
-// 	port := os.Getenv("PORT")
-
-// 	if port == "" {
-// 		port = "8000"
-// 	}
-
-// 	router := gin.New()
-// 	router.Use(gin.Logger())
-// 	routes.UserRoutes(router)
-
-// 	router.Use(middleware.Authentication())
-
-// 	// API-2
-// 	router.GET("/api-1", func(c *gin.Context) {
-
-// 		c.JSON(200, gin.H{"success": "Access granted for api-1"})
-
-// 	})
-
-// 	// API-1
-// 	router.GET("/api-2", func(c *gin.Context) {
-// 		c.JSON(200, gin.H{"success": "Access granted for api-2"})
-// 	})
-
-// 	router.Run(":" + port)
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
+// //////////////////////////////////////////////////////////////////////////////////////////	uji coba 3
 // package main
 
 // import (
+// 	"fmt"
 // 	"os"
-
-// 	"golang-restaurant-management/database"
 
 // 	middleware "golang-restaurant-management/middleware"
 // 	routes "golang-restaurant-management/routes"
 
 // 	"github.com/gin-gonic/gin"
-
-// 	"go.mongodb.org/mongo-driver/mongo"
 // )
 
-// var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
-
 // func main() {
-// 	port := os.Getenv("PORT")
+// 	fmt.Println("Server is running...")
 
+// 	port := os.Getenv("PORT")
 // 	if port == "" {
 // 		port = "8000"
 // 	}
 
 // 	router := gin.New()
 // 	router.Use(gin.Logger())
-// 	routes.UserRoutes(router)
-// 	router.Use(middleware.Authentication())
 
-// 	routes.FoodRoutes(router)
-// 	routes.MenuRoutes(router)
-// 	routes.TableRoutes(router)
-// 	routes.OrderRoutes(router)
-// 	routes.OrderItemRoutes(router)
-// 	routes.InvoiceRoutes(router)
+// 	userRoutes := router.Group("/api")
+// 	{
+// 		userRoutes.GET("/users", routes.GetUsers)
+// 		userRoutes.POST("/users", routes.CreateUser)
+// 		// Tambahkan rute-rute lainnya di sini sesuai kebutuhan
+// 	}
+
+// 	// Tambahkan rute-rute lainnya di sini sesuai kebutuhan
+
+// 	router.Use(middleware.Authentication())
 
 // 	router.Run(":" + port)
 // }
