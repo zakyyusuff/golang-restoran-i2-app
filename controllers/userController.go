413a97a80cc6f57d72094b16cc47bd34782f35c2
zakymuhammadyusuf@gmail.com <zakymuhammadyusuf@gmail.com>
2023-06-12 13:55:24 +0700

    untuk sidang

--- controllers/userController.go
+++ controllers/userController.go
@@ -65,9 +65,58 @@ func GetUsers() gin.HandlerFunc {
 			log.Fatal(err)
 		}
 		c.JSON(http.StatusOK, allUsers[0])
+		// c.HTML(http.StatusOK, "login.html", allUsers) //Ditambahin
+		// c.HTML(http.StatusOK, "login.html", gin.H{
+		// 	"users": allUsers[0]["user_items"],
+		// })
 
 	}
 }
+
+// // File uji coba 1
+// func GetUsers() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+// 		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+// 		if err != nil || recordPerPage < 1 {
+// 			recordPerPage = 10
+// 		}
+
+// 		page, err1 := strconv.Atoi(c.Query("page"))
+// 		if err1 != nil || page < 1 {
+// 			page = 1
+// 		}
+
+// 		startIndex := (page - 1) * recordPerPage
+// 		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+// 		matchStage := bson.D{{"$match", bson.D{{}}}}
+// 		projectStage := bson.D{
+// 			{"$project", bson.D{
+// 				{"_id", 0},
+// 				{"total_count", 1},
+// 				{"user_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+// 			}},
+// 		}
+
+// 		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+// 			matchStage, projectStage,
+// 		})
+// 		defer cancel()
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing user items"})
+// 			return
+// 		}
+
+// 		var allUsers []bson.M
+// 		if err = result.All(ctx, &allUsers); err != nil {
+// 			log.Fatal(err)
+// 		}
+
+// 		c.JSON(http.StatusOK, allUsers[0]["user_items"])
+// 	}
+// }
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
--- main.go
+++ main.go
@@ -43,51 +43,51 @@
 // 	router.Run(":" + port)
 // }
 
-// //////////////////////////////////////////////////////////////////////////////////////////		1
-package main
+// //////////////////////////////////////////////////////////////////////////////////////////	1 sudah bisa
+// package main
 
-import (
-	"fmt"
-	"os"
+// import (
+// 	"fmt"
+// 	"os"
 
-	middleware "golang-restaurant-management/middleware"
-	routes "golang-restaurant-management/routes"
+// 	middleware "golang-restaurant-management/middleware"
+// 	routes "golang-restaurant-management/routes"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-// var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+// // var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
-func main() {
-	fmt.Println("Server is running...")
-	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
-	// url := os.Getenv("restaurant")
-	// clientOptions := options.Client().ApplyURI(url)
-	// client, err := mongo.Connect(context.TODO(), clientOptions)
-	// port := os.Getenv(clientOptions)
+// func main() {
+// 	fmt.Println("Server is running...")
+// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
+// 	// url := os.Getenv("restaurant")
+// 	// clientOptions := options.Client().ApplyURI(url)
+// 	// client, err := mongo.Connect(context.TODO(), clientOptions)
+// 	// port := os.Getenv(clientOptions)
 
-	port := os.Getenv("PORT")
-	// ip := os.Getenv("INTERNALHOST")
+// 	port := os.Getenv("PORT")
+// 	// ip := os.Getenv("INTERNALHOST")
 
-	// if port == "" {
-	// 	port = "3000"
-	// }
+// 	// if port == "" {
+// 	// 	port = "3000"
+// 	// }
 
-	router := gin.Default()
-	router.Use(gin.Logger())
-	routes.UserRoutes(router)
-	router.Use(middleware.Authentication())
+// 	router := gin.Default()
+// 	router.Use(gin.Logger())
+// 	routes.UserRoutes(router)
+// 	router.Use(middleware.Authentication())
 
-	routes.FoodRoutes(router)
-	routes.MenuRoutes(router)
-	routes.TableRoutes(router)
-	routes.OrderRoutes(router)
-	routes.OrderItemRoutes(router)
-	routes.InvoiceRoutes(router)
+// 	routes.FoodRoutes(router)
+// 	routes.MenuRoutes(router)
+// 	routes.TableRoutes(router)
+// 	routes.OrderRoutes(router)
+// 	routes.OrderItemRoutes(router)
+// 	routes.InvoiceRoutes(router)
 
-	router.Run(":" + port)
-	// router.Run(ip + ":" + port)
-}
+// 	router.Run(":" + port)
+// 	// router.Run(ip + ":" + port)
+// }
 
 ////////////////////////////////////////////////////////////////////////////////////////////		2 uji coba
 
@@ -95,31 +95,33 @@ func main() {
 
 // import (
 // 	"fmt"
+// 	"os"
+
 // 	middleware "golang-restaurant-management/middleware"
 // 	routes "golang-restaurant-management/routes"
-// 	"net/http"
-// 	"os"
 
 // 	"github.com/gin-gonic/gin"
 // )
 
 // func main() {
-
 // 	fmt.Println("Server is running...")
-// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/test")
-// 	// url := os.Getenv("restaurant")
-// 	// clientOptions := options.Client().ApplyURI(url)
-// 	// client, err := mongo.Connect(context.TODO(), clientOptions)
-// 	// port := os.Getenv(clientOptions)
+// 	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
 
 // 	port := os.Getenv("PORT")
+// 	// ip := os.Getenv("INTERNALHOST")
 
 // 	if port == "" {
-// 		port = "8000"
+// 		port = "3000"
 // 	}
-
-// 	router := gin.New()
+// 	router := gin.Default()
 // 	router.Use(gin.Logger())
+
+// 	// Set the HTML templates directory
+// 	// router.SetHTMLTemplate(template.Must(template.ParseGlob("templates/*")))
+// 	// router.SetHTMLTemplate(template.Must(template.ParseGlob("templates/*.html")))
+// 	router.LoadHTMLGlob("templates/*.html")
+// 	// router.LoadHTMLGlob("login.html")
+
 // 	routes.UserRoutes(router)
 // 	router.Use(middleware.Authentication())
 
@@ -133,69 +135,53 @@ func main() {
 // 	router.Run(":" + port)
 // }
 
-// func Handler(c *gin.Context) {
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "Hello From Vercel !!!",
-// 	})
-// }
-
-// // func Handler(w http.ResponseWriter, r *http.Request) {
-// // 	server := New()
-
-// // 	server.get("/", func(context *Context) {
-// // 		context.JSON(200, H{
-// // 			"message": "Hello From Vercel !!!"})
-// // 	})
-
-// // 	// server.get("/users",func(context *Context) {
-// // 	// 	name := context.Query("name")
-// // 	// 	if name = "" {
-// // 	// 		context.JSON(400,H{
-// // 	// 			"message":"Hello From Vercel !!!"})
-// // 	// 	}else{
-// // 	// 		context.JSON(400,H{
-// // 	// 			"message":fmt.Sprintf("hai %s", name)})
-// // 	// 	}
-// // 	// })
-// // }
-
 // //////////////////////////////////////////////////////////////////////////////////////////	uji coba 3
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"os"
+import (
+	"fmt"
+	"os"
 
-// 	middleware "golang-restaurant-management/middleware"
-// 	routes "golang-restaurant-management/routes"
+	middleware "golang-restaurant-management/middleware"
+	routes "golang-restaurant-management/routes"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/gin-gonic/gin"
+)
 
-// func main() {
-// 	fmt.Println("Server is running...")
+func main() {
+	fmt.Println("Server is running...")
+	os.Setenv("restaurant", "mongodb+srv://zakymuhammadyusuf:zaky123@zaky.oy6yt60.mongodb.net/")
+	port := "8000"
 
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "8000"
-// 	}
+	router := gin.Default()
 
-// 	router := gin.New()
-// 	router.Use(gin.Logger())
+	// Menambahkan pengaturan CORS
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	})
 
-// 	userRoutes := router.Group("/api")
-// 	{
-// 		userRoutes.GET("/users", routes.GetUsers)
-// 		userRoutes.POST("/users", routes.CreateUser)
-// 		// Tambahkan rute-rute lainnya di sini sesuai kebutuhan
-// 	}
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
 
-// 	// Tambahkan rute-rute lainnya di sini sesuai kebutuhan
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
 
-// 	router.Use(middleware.Authentication())
+	router.Run(":" + port)
+}
 
-// 	router.Run(":" + port)
-// }
 //////////////////////////////////////////////////////////
 
 // package main
